44cde810535c412518903743d0aa5e6a01b28ced
JaeMeDev <jaeme0406@gmail.com>
2021-11-16 19:11:23 +0900

    [ADD] Context 를 사용해서 컴포넌트에 데이터 제공하기

--- 6. React Context를 이용한 상품 가격 처리/react-shop-test/src/App.js
+++ 6. React Context를 이용한 상품 가격 처리/react-shop-test/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import OrderPage from "./pages/OrderPage/OrderPage";
+import { OrderContextProvider } from "./contexts/OrderContext";
 import "./App.css";
 
 function App() {
+  let value = {
+    a: "a",
+    b: "b",
+  };
+
   return (
     <div style={{ padding: "4rem" }}>
-      <OrderPage />
+      <OrderContextProvider>
+        <OrderPage />
+      </OrderContextProvider>
     </div>
   );
 }
