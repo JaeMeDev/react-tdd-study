8cc752da0e307e5291853260777e0c634a946ebd
JaeMeDev <jaeme0406@gmail.com>
2021-11-16 19:30:30 +0900

    [ADD] context wrapper 추가로 에러 제거하기

--- 6. React Context를 이용한 상품 가격 처리/react-shop-test/src/pages/OrderPage/tests/calculate.test.js
+++ 6. React Context를 이용한 상품 가격 처리/react-shop-test/src/pages/OrderPage/tests/calculate.test.js
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Type from "../Type";
 import userEvent from "@testing-library/user-event";
+import { OrderContextProvider } from "../../../contexts/OrderContext";
 
 test("update product's total when products change", async () => {
-  render(<Type orderType="products" />);
+  render(<Type orderType="products" />, { wrapper: OrderContextProvider });
 
   // 상품 총 가격 뒤에 어떤 텍스트가 있어도 불러온다.
-  const productsTotal = screen.getByText("상품 총 가격", { exact: false });
+  const productsTotal = screen.getByText("총 가격", { exact: false });
   expect(productsTotal).toHaveTextContent("0");
 
   // 아메리카 여행 상품 한개 올리기 / 서버에서 데이터 가져온 이후는 get이 아닌 find를 사용한다.
